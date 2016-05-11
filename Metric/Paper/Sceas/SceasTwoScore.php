af52b5d0a03f7a7dab66d2f1d11c3f9426fbd4b2
Eleni Fragkiadaki <eleni.fra@gmail.com>
2016-05-11 08:09:35 +0100

    Adding the SceasTwoScore for the papers participatins to a Paper-Citation graph

---
+++ Metric/Paper/Sceas/SceasTwoScore.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Efrag\Lib\BiblioMetrics\Metric\Paper\Sceas;
+
+/**
+ * Class SceasTwoScore
+ * @package Efrag\Lib\BiblioMetrics\Metric\Paper\Sceas
+ */
+class SceasTwoScore extends SceasMetric
+{
+    /**
+     * @var float
+     */
+    protected $factorD = 0.85;
+
+    /**
+     * @param $factorD
+     * @return $this
+     */
+    public function setFactorD($factorD)
+    {
+        if (!is_numeric($factorD)) {
+            throw new \InvalidArgumentException('Factor D should be either a float or an integer');
+        }
+
+        $factorD = (float) $factorD; // convert the numeric value to a float
+
+        $this->factorD = round($factorD, $this->decimalPlaces);
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function isInitialized()
+    {
+        return parent::isInitialized() && isset($this->factorD);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function generateScores()
+    {
+        $numPapers  = count($this->paperCitations);
+        $paperIds   = array_keys($this->paperCitations);
+
+        $scores = array_fill_keys($paperIds, 1/$numPapers);
+
+        $first      = true;
+        $changed    = 0;
+        $iterations = 0;
+
+        while (($changed > 0 || $first === true) && $iterations < $this->maxIterations) {
+            $first      = false;
+            $changed    = 0;
+            $prevScores = $scores;
+
+            foreach ($scores as $paperId => $paperLastScore) {
+                $paperScoreNew = (1 - $this->factorD) + $this->factorD * $this->sumScore($paperId, $prevScores);
+
+                if ($paperScoreNew != $paperLastScore) {
+                    $changed += 1;
+                    $scores[$paperId] = $paperScoreNew;
+                }
+            }
+
+            $iterations += 1;
+        }
+
+        return $scores;
+    }
+}
